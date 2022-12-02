1d930aed47409ff9e4eca35e96dec4418ad17c3c
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-12-01 22:36:58 -0300

    feat: student crud's input validation

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateStudentDto {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   name: string;
 
+  @IsNumber()
+  @IsPositive()
   @ApiProperty()
   registration_number: number;
 }
